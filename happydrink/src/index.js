598da2fe2cba79e3e3344faffe1d167e0bf25bff
jl31150 <nafrayo@gmail.com>
2017-03-20 11:33:53 +0100

    leçon 6 brouillon

--- happydrink/src/index.js
+++ happydrink/src/index.js
@@ -12,16 +12,25 @@ import './css/index.css';
 
 import { persistStore, autoRehydrate } from 'redux-persist'
 
+import { Router, Route,IndexRoute, browserHistory }     from 'react-router'
+import { syncHistoryWithStore }                         from 'react-router-redux'
+
 const store = createStore(allReducers, undefined, autoRehydrate())
 
 persistStore(store)
 //persistStore(store, {blacklist: ['app']})// on indique le nom de la clé du reducer à blacklister
-//persistStore(store).purge() : si vous voulez "purger" ce que vous avez enregistré
+//persistStore(store).purge()// : si vous voulez "purger" ce que vous avez enregistré
 
 ReactDOM.render(
-    <Provider store={ store }>
 
-        <AppContainer title="HappyDrink"/>
+    <Provider store={ store }>
+{/*  On créé notre router en lui passant en paramètre notre historique modifié \*/}
+        <Router history={history}>
+
+            {/* On définit notre route \*/}
+            <Route path="/" component={(props) => <AppContainer {...props} title="HappyDrink"/>}></Route>
+
+        </Router>
 
     </Provider>,
 
