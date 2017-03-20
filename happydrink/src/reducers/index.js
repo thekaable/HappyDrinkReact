767b4d5a5b25acdb753cd44f0e916bdc973fdfb1
jl31150 <nafrayo@gmail.com>
2017-03-20 11:34:41 +0100

    Revert "leçon 6 brouillon"
    This reverts commit 598da2fe2cba79e3e3344faffe1d167e0bf25bff.

--- happydrink/src/reducers/index.js
+++ happydrink/src/reducers/index.js
@@ -3,12 +3,9 @@ import { combineReducers }  from 'redux'
 import appReducer           from './appReducer'
 import establishmentReducer from './establishmentReducer'
 
-import { routerReducer }  from 'react-router-redux'
-
 const allReducers = combineReducers({
     app : appReducer,
-    establishments : establishmentReducer,
-    routing : routerReducer
+    establishments : establishmentReducer
 })
 
 export default allReducers
